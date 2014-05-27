025b1649624b40a7159d1486ecea4fab6d44572a
ralicagulubova <rgylybova@melontech.com>
2014-05-27 15:12:36 +0300

    part 1-6

---
+++ polls/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+# encoding: utf8
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Question',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('question_text', models.CharField(max_length=200)),
+                ('pub_date', models.DateTimeField(verbose_name='date published')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Choice',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('question', models.ForeignKey(to='polls.Question', to_field='id')),
+                ('choice_text', models.CharField(max_length=200)),
+                ('votes', models.IntegerField(default=0)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
---
+++ polls/models.py
@@ -0,0 +1,24 @@
+import datetime
+from django.utils import timezone
+from django.db import models
+
+# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.question_text
+
+    def was_published_recently(self):
+    	now = timezone.now()
+    	return now - datetime.timedelta(days=1) <= self.pub_date < now
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.choice_text
